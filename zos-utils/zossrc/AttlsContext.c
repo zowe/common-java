dbe7f428899454e3cb176cb1333dd55f2285c5f3
Pablo Carle <pablo.carle@broadcom.com>
2025-03-14 15:25:39 +0100

    wip fixes
    Signed-off-by: Pablo Carle <pablo.carle@broadcom.com>

--- zos-utils/zossrc/AttlsContext.c
+++ zos-utils/zossrc/AttlsContext.c
@@ -527,7 +527,7 @@ Context *getContext(JNIEnv *env, jobject obj, jboolean loadCertificate, jboolean
 
     // obtain certificate buffer or create new one if needed
     c -> load_certificate = loadCertificate;
-    if (!c -> load_certificate) {
+    if (!(c -> load_certificate)) {
         c -> load_certificate = (*env) -> GetBooleanField(env, obj, always_load_certificate_field);
     }
     if (c -> load_certificate) {
@@ -545,6 +545,7 @@ Context *getContext(JNIEnv *env, jobject obj, jboolean loadCertificate, jboolean
         c -> certificate_buffer = (*env) -> GetByteArrayElements(env, certArray, 0);
         c -> certificate_buffer_length = buffer_certificate_size;
         c -> ioctl_buffer -> TTLSi_BufferPtr = c -> certificate_buffer;
+        c -> ioctl_buffer -> TTLSi_BufferLen = buffer_certificate_size;
 
         if (erase && !emptyCertificateArray) {
             memset(c -> certificate_buffer, 0, buffer_certificate_size);
@@ -554,6 +555,7 @@ Context *getContext(JNIEnv *env, jobject obj, jboolean loadCertificate, jboolean
         c -> certificate_buffer = NULL;
         c -> certificate_buffer_length = 0;
         c -> ioctl_buffer -> TTLSi_BufferPtr = NULL;
+        c -> ioctl_buffer -> TTLSi_BufferLen = 0;
 
         if (erase) {
             // current call does not require certificate buffer, but it maybe exists. Clean up it for a next call
@@ -870,7 +872,7 @@ JNIEXPORT jbyteArray JNICALL Java_org_zowe_commons_attls_AttlsContext_getCertifi
     if (! ((*env) -> ExceptionCheck(env))) {
         out = (*env) -> NewByteArray(env, c -> ioctl_buffer -> TTLSi_Cert_Len);
         if (out) {
-            (*env) -> SetByteArrayRegion(env, out, 0, c -> certificate_buffer_length, c -> certificate_buffer);
+            (*env) -> SetByteArrayRegion(env, out, 0, c -> ioctl_buffer -> TTLSi_Cert_Len, c -> certificate_buffer);
             (*env) -> SetObjectField(env, obj, certificate_cache_field, out);
         }
     }
